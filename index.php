c5b2bc79346e1b7d47ea953dbe03072eae84a4d2
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-08 16:23:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,11 +47,13 @@
 				<img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:90px;width:200px;margin-top:5px;">
 			</div>
 			<div class="inner-container">
-				<input type="text" name="emailid" placeholder="Email Id" class="credentials" value="<?php echo $email;?>">
-				<input type="password" name="password" placeholder="Password" class="credentials" value="<?php echo $password;?>" style=" margin-top:5%;">
+				<form action="home.php" method="post">
+					<input type="text" name="emailid" placeholder="Email Id" class="credentials" value="<?php echo $email;?>">
+					<input type="password" name="password" placeholder="Password" class="credentials" value="<?php echo $password;?>" style=" margin-top:5%;">
 				
-				<input type="submit" name="submit" value="Log In" class="credentials" style=" margin-top:5%; text-align:center; background-color:#04C5F9; color:white; ">				
-				<p>Not a member? &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <a href="signup.php" style="color:black;">SignUp Here</a></p>
+					<input type="submit" name="submit" value="Log In" class="credentials" style=" margin-top:5%; text-align:center; background-color:#04C5F9; color:white; ">				
+					<p>Not a member? &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <a href="signup.php" style="color:black;">SignUp Here</a></p>
+				</form>
 			</div>
 		</div>
 	</body>
