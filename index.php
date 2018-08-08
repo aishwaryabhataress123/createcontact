cc48efe90e141781ec78d2d0abb0aec7b65a58f0
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-08 14:44:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -70,11 +70,8 @@
   		echo "An error occurred.\n";
   		exit;	
 	}
-	else
-	{
 		echo " Database connected sucessfully ";
 		$query = "INSERT INTO salesforce.Contact(FirstName, LastName,Phone, MobilePhone, Email, Password__c,User_Key__c) VALUES('$_POST[firstname]','$_POST[lastname]','$_POST[phonenumber]', '$_POST[mobilenumber]','$_POST[emailid]','$_POST[password]',$_POST[userkey]);";
 		$result= pg_query($query);
-	}
-	return $db;
+		return $db;
 ?> 
