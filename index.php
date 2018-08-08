50f66673e7bf10b7a8d8b8038eb5caf6c501987a
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-08 14:38:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 </body>
 </html>
 <?php
-	$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
+	$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba442");
 	if (!$db) {
   		echo "An error occurred.\n";
   		exit;	
