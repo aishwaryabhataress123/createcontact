9c5cd7a60f200da67ba9f5e00482f109dd7d9b91
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-10 18:10:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 			<img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:90px;width:200px;margin-top:5px;">
 	</div>
 		<div class="inner-container">
-			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+			<form action="validate.php" method="post">
 				<input type="text" name="email" placeholder="Email Id" class="credentials" value="<?php echo $email;?>">
 				<input type="password" name="password" placeholder="Password" class="credentials" value="<?php echo $password;?>" style=" margin-top:5%;">
 
@@ -58,17 +58,3 @@
 	</div>
 </body>
 </html>
-<?php
-$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
-if (!$db) 
-{
-	echo "An error occurred.\n";
-	exit;	
-}
-session_start();
-$query = "SELECT Email , Password__c FROM salesforce.contact WHERE Email = '$_POST[email]' AND Password__c = '$_POST[password]';";
-$result= pg_query($query);	
-            $_SESSION['email'] = $_POST['email'];
-            $_SESSION['password'] = $_POST['password'];
-            header('Location: validate.php');
-?>
