d700c202039fdc6b943884554258f434a22577e0
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-13 10:54:09 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -16,12 +16,12 @@ if (!$db)
 $query = "SELECT Email , Password__c FROM salesforce.contact WHERE Email = $email AND Password__c = $password;";
 $result= pg_query($query);	
 $row = pg_fetch_row($query);
-if($row)
+if($row <> 1)
 {
-    	echo "You are a validated user.";
+    	echo "You are not a validated user.";
 }
 else
 {
-	echo "You are not a validated user ";
+	echo "You are a validated user ";
 }	
 ?>
