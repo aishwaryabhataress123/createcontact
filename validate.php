497a0fdbbf9b3fd18b1786026f2531148108cdd4
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-13 14:26:59 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -14,13 +14,10 @@ if (!$db)
 $query = "SELECT Email , Password__c FROM salesforce.contact;";
 $result= pg_query($query);
 $row = pg_fetch_row($result);
-while($row)
+while($row['Email'] == $_POST['email'] && $row['Password__c'] == $_POST['password'])
 {
-	if($row['Email'] == $_POST['email'] && $row['Password__c'] == $_POST['password'])
-	{
 		echo "You are a validated user/n";
 		echo "HELLO" .$_POST['email'];
 		exit;
-	}
 }
 ?>
