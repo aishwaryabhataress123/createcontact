562b7ae596a9a9ad7dcb4a22bf96849d3389ede7
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-13 16:02:58 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -28,6 +28,7 @@ echo "<br>";
 	echo "HELLO" .$_POST['email'];
 	exit;
 }*/
+echo "=====================================>" .$row.size;
 for($i=0 ; $i<$row.length ; $i++)
 {
 	if($row[0] == $_POST['email'] && $row[1] == $_POST['password'] )
