f2cc77100ecbd686df7873ff7280ddb775df501b
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-13 15:32:27 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -3,7 +3,7 @@
 // Grab User submitted information
 
 // Connect to the database
-$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
+/*$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
 if (!$db) 
 {
 	echo "An error occurred.\n";
@@ -28,9 +28,31 @@ echo "<br>";
 	echo "HELLO" .$_POST['email'];
 	exit;
 }*/
-for(i=0;i<$row.length;i++)
+/*for(i=0;i<$row.length;i++)
 {
 	if($row[0] == $_POST['email'] && $row[1] == $_POST['password'] )
+	{
+		echo "You are a validated user/n";
+		echo "HELLO" .$_POST['email'];
+		exit;
+	}
+}*/
+
+// Grab User submitted information
+// Connect to the database
+$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
+if (!$db) 
+{
+	echo "An error occurred.\n";
+	exit;	
+}
+// Select the database to use
+$query = "SELECT Email , Password__c FROM salesforce.contact;";
+$result= pg_query($query);
+$row = pg_fetch_row($result)
+while($row)
+{
+	if($row[0] == $_POST['email'] && $row[1] == $_POST['password'])
 	{
 		echo "You are a validated user/n";
 		echo "HELLO" .$_POST['email'];
