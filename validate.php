988d95c7c32b0b9682217ff90cd6f6596b3e38c3
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-13 16:15:23 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -28,7 +28,7 @@ echo "<br>";
 	echo "HELLO" .$_POST['email'];
 	exit;
 }*/
-echo sizeof($query);
+echo sizeof( pg_fetch_row($result));
 for($i=0 ; $i<sizeof($result) ; $i++)
 {
 	if($row[0] == $_POST['email'] && $row[1] == $_POST['password'] )
