9c67f486a55fdfa67b06e46809d1aef21ccb6299
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-13 12:33:50 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -1,7 +1,6 @@
 <?php
 
 // Grab User submitted information
-session_start();
 
 // Connect to the database
 $db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
@@ -15,6 +14,7 @@ if (!$db)
 $query = "SELECT Email , Password__c FROM salesforce.contact;";
 $result= pg_query($query);
 $row = pg_fetch_row($result);
+session_start();
 echo "helloooooooooo" .$_SESSION[email];
 /*while($row)
 {
