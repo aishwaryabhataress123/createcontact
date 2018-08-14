debff81cebfedc066f6014a7b50e16e0f3292e00
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-14 18:46:23 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -23,7 +23,12 @@ while($row = pg_fetch_row($result))
 	}
 }
 if($flag == 0)
-	header("Location:index.php");
+{
+	echo "<script type='text/javascript'>alert("Wrong Username or Password");
+		window.location='index.php';
+		</script>";
+	//header("Location:index.php");
+}
 // Grab User submitted information
 // Connect to the database
 /*$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
