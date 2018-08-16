86a9254a9596a915b527a66cb1d56c5201fe5fbe
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 18:15:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -46,10 +46,7 @@ $email = $password = "";
 $emailErr = $pwdErr = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 if (empty($_POST["email"])) {
-	if(!preg_match("/^\S+@[\w\d.-]{2,}\.[\w]{2,6}$/iU",$_POST["email"]))
-	{
-		$emailErr = "Email is required";
-	}
+	$emailErr = "Email is required";
 } else {
 $email = test_input($_POST["email"]);
 // check if e-mail address is well-formed
