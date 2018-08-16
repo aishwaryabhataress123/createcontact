0b1de8bfe6d5d3d68afe18cc9469cf9e0341fb3a
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 10:54:56 +0530

    Update home.php

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
 <body>
 <div class="header">
 <div class="logo">
-  <img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:50px;width:50px;">
+  <img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:100px;width:100px;">
 </div>
 </div>
 <div class="menu">
