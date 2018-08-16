9bd97b8e805eb77d3c91e7be608202dfdb8cb856
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 14:30:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -45,44 +45,47 @@ text-align:center;
 $email = $password = "";
 $emailErr = $pwdErr = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
+if (empty($_POST["email"])) {
+	if(! preg_match('/^\S+@[\w\d.-]{2,}\.[\w]{2,6}$/iU', $email))
+	{
+		$emailErr = "Email is required";
 	}
-  }
-	if (empty($_POST["password"])) {
-    $pwdErr = "Password is required";
-  } else {
-    $password = test_input($_POST["password"]);
-  }
+} else {
+$email = test_input($_POST["email"]);
+// check if e-mail address is well-formed
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+$emailErr = "Invalid email format"; 
 }
-	
+}
+if (empty($_POST["password"])) {
+$pwdErr = "Password is required";
+} else {
+$password = test_input($_POST["password"]);
+}
+}
+
 function test_input($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
 }
 ?>
 <body style="background-color:#04C5F9">
 <div class="container">
 <div class="signin">
-	<img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:90px;width:200px;margin-top:5px;">
+<img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:90px;width:200px;margin-top:5px;">
 </div>
 <div class="inner-container">
-	<form method="post" action="validate.php">
-		<input type="text" name="email" placeholder="Email Id" class="credentials">
-		<span class="error"><?php echo $emailErr;?></span>
-		<input type="password" name="password" placeholder="Password" class="credentials" style=" margin-top:5%;">
-		<span class="error"><?php echo $pwdErr;?></span>
+<form method="post" action="validate.php">
+	<input type="text" name="email" placeholder="Email Id" class="credentials">
+	<span class="error"><?php echo $emailErr;?></span>
+	<input type="password" name="password" placeholder="Password" class="credentials" style=" margin-top:5%;">
+	<span class="error"><?php echo $pwdErr;?></span>
 
-		<input type="submit" name="submit" value="Log In" class="credentials" style=" margin-top:5%; text-align:center; background-color:#04C5F9; color:white; ">				
-		<p>Not a member? &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <a href="signup.php" style="color:black;">SignUp Here</a></p>
-	</form>
+	<input type="submit" name="submit" value="Log In" class="credentials" style=" margin-top:5%; text-align:center; background-color:#04C5F9; color:white; ">				
+	<p>Not a member? &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <a href="signup.php" style="color:black;">SignUp Here</a></p>
+</form>
 </div>
 </div>
 </body>
