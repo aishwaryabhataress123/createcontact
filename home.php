336f26f842e80c633296cc3238f9a8ac399e0ac0
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 10:47:16 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,24 +1,29 @@
 <html>
 <head>
 <style>
-  .header{
-  
-  }
-  .logo{
+.header{
 
-  }
-  .body{
-  
-  }
+}
+.logo{
+
+}
+.menu{
+  background-color:black; 
+}
+.mainframe{
+
+}
 </style>
 </head>
 <body>
 <div class="header">
-  <div class="logo">
-    <img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:50px;width:50px;">
-  </div>
- </div>
-<div class="body">
+<div class="logo">
+  <img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture" style="height:50px;width:50px;">
+</div>
+</div>
+<div class="menu">
+</div>
+<div class="mainframe">
 </div>
 </body>
 </html>
