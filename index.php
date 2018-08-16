ed939caffb1221332507c587181fb315b0afe92f
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 12:24:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -52,9 +52,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $email = test_input($_POST["email"]);
     // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
   }
 }
 	
