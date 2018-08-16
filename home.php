6a0c9b48c052e363541bcd214181221f88e5f105
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 10:35:48 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,13 +1,15 @@
 <html>
 <head>
-  <style>
-  </style>
+<style>
+</style>
 </head>
 <body>
-  <div class="header">
-    <img src=""
+<div class="header">
+  <div class="logo">
+    <img src="https://image3.mouthshut.com/images/imagesp/925609709s.jpg" alt="Aress Picture">
   </div>
-  <div class="body">
-  </div>
+ </div>
+<div class="body">
+</div>
 </body>
 </html>
