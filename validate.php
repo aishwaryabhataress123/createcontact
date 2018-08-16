cf97f3e9dbb8fcd170ac8c1f08936495cf949873
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-16 14:20:27 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -1,29 +1,4 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-	}
-  }
-	if (empty($_POST["password"])) {
-    $pwdErr = "Password is required";
-  } else {
-    $password = test_input($_POST["password"]);
-  }
-}
-	
-function test_input($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-}
-?>
-<?php
 
 // Grab User submitted information
 
