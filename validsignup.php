01778fa42f40917c129aef00115291aa6f0b9fad
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-17 15:42:55 +0530

    Update validsignup.php

--- validsignup.php
+++ validsignup.php
@@ -21,6 +21,6 @@ while($row = pg_fetch_row($result))
 if($flag == 0)
 {
 	echo "<script type='text/javascript'>alert(\"Sign Up Failed! Enter proper emailid and userkey\")</script>";
-	header("Location:signup.php");
+	include("signup.php");
 }
 ?>
