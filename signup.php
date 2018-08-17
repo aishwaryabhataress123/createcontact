e9f550f548f88cb6da0e248bfd6e795703f01430
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-17 14:39:57 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@
                alert("Passwords do no match");
                return false;
             }
-    var x = document.forms["signup"]["email"].value;
+    var x = document.forms["signup"]["emailid"].value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
