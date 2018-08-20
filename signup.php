d95a3158cefb9a4dae46f0704dcdbf79e13ca5bb
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-08-20 11:54:54 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -104,12 +104,3 @@ $lnameErr = "Only letters and white space allowed";
 </div>
 </body>
 </html>
-<?php
-	$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
-	if (!$db) {
-		echo "An error occurred.\n";
-		exit;	
-	}
-	$query = "INSERT INTO salesforce.contact(FirstName, LastName,Phone, MobilePhone, Email, Password__c,User_Key__c) VALUES('$_POST[firstname]','$_POST[lastname]','$_POST[phonenumber]', '$_POST[mobilenumber]','$_POST[emailid]','$_POST[password]','$_POST[userkey]');";
-	$result= pg_query($query);
-?> 
